a0d7dcced94829df7047cc85a401b81d64ce6bd5
Shvlti <131967742+Shvlti@users.noreply.github.com>
2023-07-12 01:35:58 +0600

    Add files via upload

---
+++ 12 the witcher/js/main.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    speed: 2400,
+    //direction: 'vertical',
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.4
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true
+})
